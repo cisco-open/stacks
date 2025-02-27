579736ad13d0afd006fa65a757d2a8afb20e0524
Pere Cortada <150281468+pcortada-te@users.noreply.github.com>
2025-02-27 13:37:35 +0000

    Add version option (#12)
    * Add version command
    
    * Adding documnetation and linting

--- src/stacks/main.py
+++ src/stacks/main.py
@@ -1,4 +1,6 @@
+import importlib.metadata
 import pathlib
+import platform
 
 import click
 
@@ -14,6 +16,12 @@ def cli():
     All commands MUST run within a layer directory, unless noted otherwise.
     """
     pass
+
+
+@cli.command()
+def version():
+    print(f"Stacks {importlib.metadata.version('stacks')}")
+    print(f"Python {platform.python_version()}")
 
 
 @cli.command(hidden=True)  # hidden because it should not be used independently unless for advanced debugging purposes
