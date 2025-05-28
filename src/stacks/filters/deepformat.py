7bebf0a24d031445f05922fbc626047e698edf70
Josep Medialdea <117831052+jmediald-te@users.noreply.github.com>
2025-05-28 19:26:16 +0200

    Fix deepmerge filter (#17)

--- src/stacks/filters/deepformat.py
+++ src/stacks/filters/deepformat.py
@@ -2,7 +2,7 @@ def deepformat(ctx, value, params):
     if isinstance(value, str):
         return value.format(**params)
     elif isinstance(value, list):
-        return [deepformat(item, params) for item in value]
+        return [deepformat(ctx, item, params) for item in value]
     elif isinstance(value, dict):
-        return {deepformat(key, params): deepformat(value, params) for key, value in value.items()}
+        return {deepformat(ctx, key, params): deepformat(ctx, value, params) for key, value in value.items()}
     return value
